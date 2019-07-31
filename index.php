58dd843a2bade1c152ffd5a191ee3623c9c831bf
XuanDiem <xuandiem0011@gmail.com>
2019-07-31 15:17:51 +0700

    Tim Min

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$mang = array(0, -1,-2, 3, 4, 5, 6, 7, 8);
+
+function searchMinArray($array)
+{
+    $min = $array[0];
+    for ($i = 1; $i < count($array); $i++) {
+        if ($min > $array[$i]) {
+            $min = $array[$i];
+
+        }
+        return $min;
+
+    }
+}
+
+echo searchMinArray($mang);
+?>
